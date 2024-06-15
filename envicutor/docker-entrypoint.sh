b8a1ce4e41f8c41b52bcfab277f6bbdc4c36d73f
Omar Brikaa <brikaaomar@gmail.com>
2024-06-15 11:23:08 +0300

    Initial Dockerfile

---
+++ envicutor/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /sys/fs/cgroup && \
+mkdir isolate/ && \
+echo 1 > isolate/cgroup.procs && \
+echo '+cpuset +cpu +io +memory +pids' > cgroup.subtree_control && \
+cd isolate && \
+mkdir init && \
+echo 1 > init/cgroup.procs
+echo '+cpuset +memory' > cgroup.subtree_control && \
+exec "$@"
