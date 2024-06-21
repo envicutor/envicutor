bcc7003d871eedc9bb0c59303c9784fdb154cd4c
Omar Brikaa <brikaaomar@gmail.com>
2024-06-21 12:18:25 +0300

    Test improvements, installation locking test

--- test/concurrency.js
+++ test/concurrency.js
@@ -66,6 +66,73 @@ time.sleep(1)`
     console.log(
       'Executing 32 submissions after a package installation has started (they should start after the installation)'
     );
+    sendRequest('POST', `${BASE_URL}/runtimes`, {
+      name: 'Fake lang',
+      nix_shell: `{ pkgs ? import (
+  fetchTarball {
+    url="https://github.com/NixOS/nixpkgs/archive/72da83d9515b43550436891f538ff41d68eecc7f.tar.gz";
+    sha256="177sws22nqkvv8am76qmy9knham2adfh3gv7hrjf6492z1mvy02y";
+  }
+) {} }:
+pkgs.mkShell {
+  shellHook = ''
+  sleep 1
+  exit 1
+  '';
+  nativeBuildInputs = with pkgs; [];
+}`,
+      compile_script: 'g++ main.cpp',
+      run_script: './a.out',
+      source_file_name: 'main.cpp'
+    });
+
+    await sleep(10);
+
+    const promises = [];
+    const start = new Date();
+    for (let i = 0; i < 32; ++i) {
+      promises.push(
+        (async () => {
+          await sendRequest('POST', `${BASE_URL}/execute`, {
+            runtime_id: 2,
+            source_code: `
+import time
+time.sleep(1)`
+          });
+          return new Date() - start;
+        })()
+      );
+    }
+    const before = new Date();
+    const durations = await Promise.all(promises);
+    const total_time = new Date() - before;
+    for (const duration of durations) {
+      assert.ok(duration >= 2000, 'Found a submission that finished before two seconds');
+    }
+    console.log(`Approximate time to run all submissions: ${total_time} ms`);
+  }
+
+  // Have 32 submissions that take a second running at the same time of the installation
+  // Have installation running that takes a second and fails
+  // Check that each submission finished after two seconds
+  {
+    console.log(
+      'Running a package installation after executing 32 submissions has started (it should start after the executions finish)'
+    );
+
+    const promises = [];
+    for (let i = 0; i < 32; ++i) {
+      promises.push(
+        sendRequest('POST', `${BASE_URL}/execute`, {
+          runtime_id: 2,
+          source_code: `
+import time
+time.sleep(1)`
+        })
+      );
+    }
+    await sleep(10);
+
     const start = new Date();
     await sendRequest('POST', `${BASE_URL}/runtimes`, {
       name: 'Fake lang',
@@ -87,26 +154,8 @@ pkgs.mkShell {
       source_file_name: 'main.cpp'
     });
 
-    const promises = [];
-    for (let i = 0; i < 32; ++i) {
-      promises.push(
-        (async () => {
-          await sendRequest('POST', `${BASE_URL}/execute`, {
-            runtime_id: 2,
-            source_code: `
-import time
-time.sleep(1)`
-          });
-          return new Date() - start;
-        })()
-      );
-    }
-    const before = new Date();
-    const durations = await Promise.all(promises);
-    const total_time = new Date() - before;
-    for (const duration of durations) {
-      assert.ok(duration >= 2000, 'Found a submission that finished before two seconds');
-    }
-    console.log(`Approximate time to run all submissions: ${total_time} ms`);
+    const duration = new Date() - start;
+    console.log(`Time to finish installation: ${duration}`);
+    assert.ok(duration >= 2000, 'Installation finished before 2 seconds');
   }
 })();
