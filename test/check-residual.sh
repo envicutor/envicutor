d286e85f52d3e3b6f2e58ade1624f63ca811914f
Omar Brikaa <brikaaomar@gmail.com>
2024-08-10 21:22:58 +0300

    Add cgroup directory to residual files check

--- test/check-residual.sh
+++ test/check-residual.sh
@@ -1,2 +1,2 @@
 set -x
-docker compose exec envicutor /bin/bash -c 'if [[ -z "$(ls /var/local/lib/isolate && ls /envicutor/tmp)" ]]; then exit 0; else exit 1; fi' || (echo found residual files && exit 1)
+docker compose exec envicutor /bin/bash -c 'if [[ -z "$(ls /var/local/lib/isolate && ls /envicutor/tmp && ls /sys/fs/cgroup/isolate/box-*)" ]]; then exit 0; else exit 1; fi' || (echo found residual files && exit 1)
